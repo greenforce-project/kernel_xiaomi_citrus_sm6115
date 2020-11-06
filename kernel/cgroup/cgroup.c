62efc835106db02c7fb8db4b863bd4bbd0c62f83
Hui Su <sh_def@163.com>
2020-11-06 22:47:40 +0800

    BACKPORT: cgroup/cgroup.c: replace 'of->kn->priv' with of_cft()
    we have supplied the inline function: of_cft() in cgroup.h.
    
    So replace the direct use 'of->kn->priv' with inline func
    of_cft(), which is more readable.
    
    Signed-off-by: Hui Su <sh_def@163.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    
    (cherry picked from commit 5a7b5f32c5aa628841502d19a813c633ff6ecbe4)
    
    Conflicts:
       kernel/cgroup/cgroup.c
    
    BUG=b:244260899
    TEST=CQ
    
    Signed-off-by: Zubin Mithra <zsm@google.com>
    Change-Id: Id2251f4dbfca6f5cf511fe0a0ca49a2238f428a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3872041
    Reviewed-by: Guenter Roeck <groeck@chromium.org>
    Commit-Queue: Zubin Mithra <zsm@chromium.org>
    Tested-by: Zubin Mithra <zsm@chromium.org>
    Signed-off-by: fadlyas07 <mhmmdfdlyas@gmail.com>

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -3617,7 +3617,7 @@ static ssize_t cgroup_freeze_write(struct kernfs_open_file *of,
 
 static int cgroup_file_open(struct kernfs_open_file *of)
 {
-	struct cftype *cft = of->kn->priv;
+	struct cftype *cft = of_cft(of);
 	struct cgroup_file_ctx *ctx;
 	int ret;
 
@@ -3642,7 +3642,7 @@ static int cgroup_file_open(struct kernfs_open_file *of)
 
 static void cgroup_file_release(struct kernfs_open_file *of)
 {
-	struct cftype *cft = of->kn->priv;
+	struct cftype *cft = of_cft(of);
 	struct cgroup_file_ctx *ctx = of->priv;
 
 	if (cft->release)
@@ -3656,7 +3656,7 @@ static ssize_t cgroup_file_write(struct kernfs_open_file *of, char *buf,
 {
 	struct cgroup_file_ctx *ctx = of->priv;
 	struct cgroup *cgrp = of->kn->parent->priv;
-	struct cftype *cft = of->kn->priv;
+	struct cftype *cft = of_cft(of);
 	struct cgroup_subsys_state *css;
 	int ret;
 
@@ -3706,7 +3706,7 @@ static ssize_t cgroup_file_write(struct kernfs_open_file *of, char *buf,
 
 static __poll_t cgroup_file_poll(struct kernfs_open_file *of, poll_table *pt)
 {
-	struct cftype *cft = of->kn->priv;
+	struct cftype *cft = of_cft(of);
 
 	if (cft->poll)
 		return cft->poll(of, pt);
