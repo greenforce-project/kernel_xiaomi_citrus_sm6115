2541f870e57abfb5227497c067230523891b1ca0
Adam W. Willis <return.of.octobot@gmail.com>
2021-04-11 16:39:28 -0400

    sched/boost: Boost CPU and DDR bus when sched_boost is triggered
    In addition to inhibiting CAF's sched_boost functionality, we will
    repurpose /proc/sys/kernel/sched_boost to act as a userspace hint
    for (CPU_INPUT|DEVFREQ)_BOOST to sustain boosts in order to cover
    extended user interactions which exceed the inital
    INPUT_BOOST_DURATION_MS window.
    
    Suggested-by: Yaroslav Furman <yaro330@gmail.com>
    Co-authored-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>
    [0ctobot: Squash idkwhoiam322/weeb_kernel_oneplus_sm8150@652b6ae
    with idkwhoiam322/weeb_kernel_oneplus_sm8150@2289c62 and
    idkwhoiam322/weeb_kernel_oneplus_sm8150@65ce97e]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Change-Id: Ie893cadd53d486afcc3391a12d2a01c8ab372fd2
    Signed-off-by: fadlyas07 <mhmmdfdlyas@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -33,8 +33,6 @@ static unsigned int cpu_freq_min_little __read_mostly =
 static unsigned int cpu_freq_min_big __read_mostly =
 	CONFIG_CPU_FREQ_MIN_PERF;
 
-static unsigned short input_boost_duration __read_mostly =
-	CONFIG_INPUT_BOOST_DURATION_MS;
 static unsigned short wake_boost_duration __read_mostly =
 	CONFIG_WAKE_BOOST_DURATION_MS;
 
@@ -45,7 +43,6 @@ module_param(max_boost_freq_big, uint, 0644);
 module_param(cpu_freq_min_little, uint, 0644);
 module_param(cpu_freq_min_big, uint, 0644);
 
-module_param(input_boost_duration, short, 0644);
 module_param(wake_boost_duration, short, 0644);
 
 enum {
@@ -130,12 +127,9 @@ static void __cpu_input_boost_kick(struct boost_drv *b)
 	if (test_bit(SCREEN_OFF, &b->state))
 		return;
 
-	if (!input_boost_duration)
-		return;
-
 	set_bit(INPUT_BOOST, &b->state);
 	if (!mod_delayed_work(system_unbound_wq, &b->input_unboost,
-			      msecs_to_jiffies(input_boost_duration)))
+			      msecs_to_jiffies(CONFIG_INPUT_BOOST_DURATION_MS)))
 		wake_up(&b->boost_waitq);
 }
 
