a8ace6cac676a800a231e44647e9efff6e627cff
Sultan Alsawaf <sultan@kerneltoast.com>
2021-07-04 22:35:09 -0400

    FIXUP: devfreq_boost: Introduce devfreq boost driver
    This driver boosts enumerated devfreq devices upon input, and allows for
    boosting specific devfreq devices on other custom events. The boost
    frequencies for this driver should be set so that frame drops are
    near-zero at the boosted frequencies and power consumption is minimized
    at said frequencies. The goal of this driver is to provide an interface
    to achieve optimal device performance by requesting boosts on key
    events, such as when a frame is ready to rendered to the display.
    
    Currently, support is only present for boosting the cpubw devfreq
    device, but the driver is structured in a way that makes it easy to add
    support for new boostable devfreq devices in the future.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [0ctobot: Bring inline with kerneltoast/android_kernel_google_floral@dce5aa5,
    amending the following commits where applicable:
    076de0a ("[2/2] devfreq_boost: Rework display state awareness logic for OnePlus 8 devices")
    bf07438 ("devfreq_boost: Update and expand to handle CPUBW/LLCCBW boosting")
    2ca6410 ("devfreq_boost: Mark boost kthreads as performance critical")]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Change-Id: Iffda967c6a61a5e5d98f49bbd2be780b1cf4b705
    Signed-off-by: fadlyas07 <mhmmdfdlyas@gmail.com>

--- techpack/display/msm/sde/sde_crtc.c
+++ techpack/display/msm/sde/sde_crtc.c
@@ -1,6 +1,5 @@
 /*
  * Copyright (c) 2014-2021 The Linux Foundation. All rights reserved.
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  * Copyright (C) 2013 Red Hat
  * Author: Rob Clark <robdclark@gmail.com>
  *
@@ -380,34 +379,14 @@ static ssize_t vsync_event_show(struct device *device,
 			ktime_to_ns(sde_crtc->vblank_last_cb_time));
 }
 
-static ssize_t retire_frame_event_show(struct device *device,
-	struct device_attribute *attr, char *buf)
-{
-	struct drm_crtc *crtc;
-	struct sde_crtc *sde_crtc;
-
-	if (!device || !buf) {
-		SDE_ERROR("invalid input param(s)\n");
-		return -EAGAIN;
-	}
-
-	crtc = dev_get_drvdata(device);
-	sde_crtc = to_sde_crtc(crtc);
-	SDE_EVT32(DRMID(&sde_crtc->base));
-	return scnprintf(buf, PAGE_SIZE, "RETIRE_FRAME_TIME=%llu\n",
-			ktime_to_ns(sde_crtc->retire_frame_event_time));
-}
-
 static DEVICE_ATTR_RO(vsync_event);
 static DEVICE_ATTR_RO(measured_fps);
 static DEVICE_ATTR_RW(fps_periodicity_ms);
-static DEVICE_ATTR_RO(retire_frame_event);
 
 static struct attribute *sde_crtc_dev_attrs[] = {
 	&dev_attr_vsync_event.attr,
 	&dev_attr_measured_fps.attr,
 	&dev_attr_fps_periodicity_ms.attr,
-	&dev_attr_retire_frame_event.attr,
 	NULL
 };
 
@@ -431,8 +410,6 @@ static void sde_crtc_destroy(struct drm_crtc *crtc)
 
 	if (sde_crtc->vsync_event_sf)
 		sysfs_put(sde_crtc->vsync_event_sf);
-	if (sde_crtc->retire_frame_event_sf)
-		sysfs_put(sde_crtc->retire_frame_event_sf);
 	if (sde_crtc->sysfs_dev)
 		device_unregister(sde_crtc->sysfs_dev);
 
@@ -2186,12 +2163,6 @@ static void sde_crtc_frame_event_cb(void *data, u32 event)
 		}
 	}
 
-	if ((event & SDE_ENCODER_FRAME_EVENT_SIGNAL_RETIRE_FENCE) &&
-		(sde_crtc && sde_crtc->retire_frame_event_sf)) {
-		sde_crtc->retire_frame_event_time = ktime_get();
-		sysfs_notify_dirent(sde_crtc->retire_frame_event_sf);
-	}
-
 	fevent->event = event;
 	fevent->crtc = crtc;
 	fevent->connector = cb_data->connector;
@@ -3188,7 +3159,6 @@ static void sde_crtc_atomic_begin(struct drm_crtc *crtc,
 		if (encoder->crtc != crtc)
 			continue;
 
-		sde_encoder_trigger_rsc_state_change(encoder);
 		/* encoder will trigger pending mask now */
 		sde_encoder_trigger_kickoff_pending(encoder);
 	}
@@ -3657,8 +3627,6 @@ void sde_crtc_commit_kickoff(struct drm_crtc *crtc,
 		SDE_EVT32(DRMID(crtc), SDE_EVTLOG_FUNC_CASE2);
 	}
 	sde_crtc->play_count++;
-
-	sde_vbif_clear_errors(sde_kms);
 
 	if (is_error) {
 		_sde_crtc_remove_pipe_flush(crtc);
@@ -6372,12 +6340,6 @@ int sde_crtc_post_init(struct drm_device *dev, struct drm_crtc *crtc)
 	if (!sde_crtc->vsync_event_sf)
 		SDE_ERROR("crtc:%d vsync_event sysfs create failed\n",
 						crtc->base.id);
-
-	sde_crtc->retire_frame_event_sf = sysfs_get_dirent(
-		sde_crtc->sysfs_dev->kobj.sd, "retire_frame_event");
-	if (!sde_crtc->retire_frame_event_sf)
-		SDE_ERROR("crtc:%d retire frame event sysfs create failed\n",
-			crtc->base.id);
 
 end:
 	return rc;
