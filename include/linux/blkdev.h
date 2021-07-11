5603274b51de5a3a4128e0524f22eaf89f5da201
root <alexfinhart@gmail.com>
2021-07-11 09:36:25 +0800

    blkdev: switch to SSD mode * optimize to flash storage Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -717,6 +717,7 @@ struct request_queue {
 #define QUEUE_FLAG_QUIESCED    28	/* queue has been quiesced */
 
 #define QUEUE_FLAG_DEFAULT	((1 << QUEUE_FLAG_SAME_COMP)	|	\
+				 (1 << QUEUE_FLAG_NONROT)	|	\
 				 (0 << QUEUE_FLAG_ADD_RANDOM))
 
 #define QUEUE_FLAG_MQ_DEFAULT	((1 << QUEUE_FLAG_SAME_COMP)	|	\
