c7c57b7df0ae27134ebda71d368a08ae459874a8
Adam W. Willis <return.of.octobot@gmail.com>
2021-08-25 17:18:52 -0400

    [HACK] msm: ipa: Enhance IPA_EMULATION guarding to avoid undefined references
    When compiling the kernel using GCC LTO, we hit the following errors at link time:
    
    /usr/lib/gcc/aarch64-linux-gnu/11.2.0/../../../../aarch64-linux-gnu/bin/ld: /tmp/cclIai5s.ltrans87.ltrans.o: in function `ipa_pci_remove':
    <artificial>:(.text+0xfc): undefined reference to `ipa_ut_module_exit'
    /usr/lib/gcc/aarch64-linux-gnu/11.2.0/../../../../aarch64-linux-gnu/bin/ld: /tmp/cclIai5s.ltrans87.ltrans.o: in function `ipa_pci_probe':
    <artificial>:(.text+0x2310c): undefined reference to `ipa_ut_module_init'
    collect2: error: ld returned 1 exit status
    make[1]: *** [/home/0ctobot/Android/OnePlus/kebab/neutrino_kernel_oneplus_sm8250/Makefile:1211: vmlinux] Error 1
    make: *** [Makefile:146: sub-make] Error 2
    
    This seems to be a result of DCE, as these functions become
    effectively unreachable with !CONFIG_IPA_UT and the compiler has
    optimized out code for if-statements that it can prove are always
    false.
    
    This little hack may not jibe perfectly with what the original
    author had in mind, but it's sufficient for our usecase.
    
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- drivers/platform/msm/ipa/ipa_api.c
+++ drivers/platform/msm/ipa/ipa_api.c
@@ -97,8 +97,6 @@
 
 #if defined(CONFIG_IPA_EMULATION)
 static bool running_emulation = true;
-#elif defined(CONFIG_PCI)
-static bool running_emulation;
 #endif
 
 static enum ipa_hw_type ipa_api_hw_type;
@@ -3200,7 +3198,7 @@ static const struct pci_error_handlers ipa_pci_err_handler = {
 	.resume = ipa_pci_io_resume,
 };
 
-static struct pci_driver ipa_pci_driver = {
+static struct pci_driver ipa_pci_driver __maybe_unused = {
 	.name     = ipa_pci_driver_name,
 	.id_table = ipa_pci_tbl,
 	.probe    = ipa_pci_probe,
@@ -3899,7 +3897,7 @@ static void ipa_pci_io_resume(struct pci_dev *pci_dev)
 static int __init ipa_module_init(void)
 {
 	pr_debug("IPA module init\n");
-#ifdef CONFIG_PCI
+#if defined(CONFIG_PCI) && defined(CONFIG_IPA_EMULATION)
 	if (running_emulation) {
 		/* Register as a PCI device driver */
 		return pci_register_driver(&ipa_pci_driver);
