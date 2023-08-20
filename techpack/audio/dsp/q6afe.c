6d3bf53714988069706ed78ecef325a3e5587ced
fadlyas07 <mhmmdfdlyas@proton.me>
2023-08-20 00:47:47 +0000

    Revert "asoc: update wakeup_source API"
     - Use wakeup_source_init and wakeup_source_trash again as they are still available here, revert this commit when they have completely disappeared.
    
    This reverts commit 34b9005296bef7b9de716f9a6692b4c38466ae7b.
    
    Change-Id: Ia948b4f6cfc94c950930e4d94447a9f44d9d9e67

--- techpack/audio/dsp/q6afe.c
+++ techpack/audio/dsp/q6afe.c
@@ -10,7 +10,6 @@
 #include <linux/jiffies.h>
 #include <linux/sched.h>
 #include <linux/delay.h>
-#include <linux/version.h>
 #include <dsp/msm_audio_ion.h>
 #include <dsp/apr_audio-v2.h>
 #include <dsp/audio_cal_utils.h>
@@ -122,7 +121,7 @@ enum {
 };
 
 struct wlock {
-	struct wakeup_source *ws;
+	struct wakeup_source ws;
 };
 
 static struct wlock wl;
@@ -9600,7 +9599,7 @@ static int afe_set_cal_fb_spkr_prot(int32_t cal_type, size_t data_size,
 		goto done;
 
 	if (cal_data->cal_info.mode == MSM_SPKR_PROT_CALIBRATION_IN_PROGRESS)
-		__pm_wakeup_event(wl.ws, jiffies_to_msecs(WAKELOCK_TIMEOUT));
+		__pm_wakeup_event(&wl.ws, jiffies_to_msecs(WAKELOCK_TIMEOUT));
 	mutex_lock(&this_afe.cal_data[AFE_FB_SPKR_PROT_CAL]->lock);
 	memcpy(&this_afe.prot_cfg, &cal_data->cal_info,
 		sizeof(this_afe.prot_cfg));
@@ -9979,7 +9978,7 @@ static int afe_get_cal_fb_spkr_prot(int32_t cal_type, size_t data_size,
 	}
 	this_afe.initial_cal = 0;
 	mutex_unlock(&this_afe.cal_data[AFE_FB_SPKR_PROT_CAL]->lock);
-	__pm_relax(wl.ws);
+	__pm_relax(&wl.ws);
 done:
 	return ret;
 }
@@ -10282,6 +10281,7 @@ int __init afe_init(void)
 	init_waitqueue_head(&this_afe.wait_wakeup);
 	init_waitqueue_head(&this_afe.lpass_core_hw_wait);
 	init_waitqueue_head(&this_afe.clk_wait);
+	wakeup_source_init(&wl.ws, "spkr-prot");
 	ret = afe_init_cal_data();
 	if (ret)
 		pr_err("%s: could not init cal data! %d\n", __func__, ret);
@@ -10291,11 +10291,7 @@ int __init afe_init(void)
 	this_afe.uevent_data = kzalloc(sizeof(*(this_afe.uevent_data)), GFP_KERNEL);
 	if (!this_afe.uevent_data)
 		return -ENOMEM;
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 19, 110))
-	wl.ws = wakeup_source_register(NULL, "spkr-prot");
-#else
-	wl.ws = wakeup_source_register("spkr-prot");
-#endif
+
 	/*
 	 * Set release function to cleanup memory related to kobject
 	 * before initializing the kobject.
@@ -10330,7 +10326,7 @@ void afe_exit(void)
 	mutex_destroy(&this_afe.afe_cmd_lock);
 	mutex_destroy(&this_afe.afe_apr_lock);
 	mutex_destroy(&this_afe.afe_clk_lock);
-	wakeup_source_unregister(wl.ws);
+	wakeup_source_trash(&wl.ws);
 }
 
 /*
