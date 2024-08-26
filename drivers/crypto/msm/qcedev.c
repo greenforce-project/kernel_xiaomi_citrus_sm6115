fdc668a0000eeaddc8e179ff2d60534bccd91ba8
fadlyas07 <mhmmdfdlyas@proton.me>
2024-08-26 08:30:36 +0000

    Revert "crypto: qcedev: Fix misleading identation"
    This reverts commit d6cee613eb2a6469653dfa8ae50ae997a694a1dc.
    
    Change-Id: I94214e217c49fec1d86cfd4b30ef9b6e6828147a

--- drivers/crypto/msm/qcedev.c
+++ drivers/crypto/msm/qcedev.c
@@ -1876,7 +1876,8 @@ static inline long qcedev_ioctl(struct file *file,
 			err = -ENOTTY;
 			goto exit_free_qcedev_areq;
 		}
-	case QCEDEV_IOCTL_SHA_UPDATE_REQ: {
+	case QCEDEV_IOCTL_SHA_UPDATE_REQ:
+		{
 		struct scatterlist sg_src;
 
 		if (copy_from_user(&qcedev_areq->sha_op_req,
@@ -1925,11 +1926,10 @@ static inline long qcedev_ioctl(struct file *file,
 				handle->sha_ctxt.diglen);
 		mutex_unlock(&hash_access_lock);
 		if (copy_to_user((void __user *)arg, &qcedev_areq->sha_op_req,
-					sizeof(struct qcedev_sha_op_req))) {
+					sizeof(struct qcedev_sha_op_req)))
 			err = -EFAULT;
 			goto exit_free_qcedev_areq;
 		}
-	}
 		break;
 
 	case QCEDEV_IOCTL_SHA_FINAL_REQ:
@@ -1977,7 +1977,8 @@ static inline long qcedev_ioctl(struct file *file,
 		handle->sha_ctxt.init_done = false;
 		break;
 
-	case QCEDEV_IOCTL_GET_SHA_REQ: {
+	case QCEDEV_IOCTL_GET_SHA_REQ:
+		{
 		struct scatterlist sg_src;
 
 		if (copy_from_user(&qcedev_areq->sha_op_req,
@@ -2017,11 +2018,10 @@ static inline long qcedev_ioctl(struct file *file,
 				handle->sha_ctxt.diglen);
 		mutex_unlock(&hash_access_lock);
 		if (copy_to_user((void __user *)arg, &qcedev_areq->sha_op_req,
-					sizeof(struct qcedev_sha_op_req))) {
+					sizeof(struct qcedev_sha_op_req)))
 			err = -EFAULT;
 			goto exit_free_qcedev_areq;
 		}
-	}
 		break;
 
 	case QCEDEV_IOCTL_MAP_BUF_REQ:
